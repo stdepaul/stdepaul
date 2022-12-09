afa93b34dc3320f2d92052bbf49e91647405a30f
Michael Johnson <stdepaulorg@gmail.com>
2022-12-09 12:34:53 -0600

    first dev commit

--- root_app/views.py
+++ root_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+	context = {
+
+	}
+	template_name = 'home.html'
+	return render(request, template_name, context)
